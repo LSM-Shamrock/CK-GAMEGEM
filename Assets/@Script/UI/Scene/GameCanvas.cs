536f1edef1123c45f9e0e54dc022fb5a8a78de43
LSM-Shamrock <lsm.shamrock@gmail.com>
2025-07-16 01:37:30 +0900

    ㅁ

---
+++ Assets/@Script/UI/Scene/GameCanvas.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class GameCanvas : UI_Scene
+{
+    public override bool Init()
+    {
+        if (base.Init() == false)
+            return false;
+
+
+
+        _init = true;
+        return true;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F11))
+            Screen.fullScreen = !Screen.fullScreen;
+    }
+}
--- Assets/@Script/UI/UI_Scene.cs
+++ Assets/@Script/UI/UI_Scene.cs
@@ -9,7 +9,17 @@ public class UI_Scene : UI_Base
 		if (base.Init() == false)
 			return false;
 
-		Manager.UI.SetCanvas(gameObject, false);
+
+        GameObject go = GameObject.Find("EventSystem");
+        if (go == null)
+        {
+            Manager.Resource.Instantiate("EventSystem", null, (go) =>
+            {
+                go.name = "EventSystem";
+            });
+        }
+
+        Manager.UI.SetCanvas(gameObject, false);
 		return true;
 	}
 }
