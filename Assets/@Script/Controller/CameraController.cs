aefc48979ee8c53f8c524ec8dc0ec3ee541a28bc
LSM-Shamrock <lsm.shamrock@gmail.com>
2025-07-16 01:07:42 +0900

    카메라 사이즈 조절 등

--- Assets/@Script/Controller/CameraController.cs
+++ Assets/@Script/Controller/CameraController.cs
@@ -2,13 +2,32 @@
 
 public class CameraController : MonoBehaviour
 {
+    private Camera _camera;
+
     private float _moveSpeed = 5f;
+
+    [SerializeField] private float dist1 = 20f;
+    [SerializeField] private float dist2 = 40f;
+    [SerializeField] private float size1 = 10f;
+    [SerializeField] private float size2 = 20f;
+
+    private void Awake()
+    {
+        _camera = GetComponent<Camera>();
+    }
 
     private void LateUpdate()
     {
-        Vector3 targetPos = Vector3.Lerp(Manager.Obj.P1.transform.position, Manager.Obj.P2.transform.position, 0.5f);
-        targetPos.y = 0f;
+        Vector3 p1Pos = Manager.Obj.P1.transform.position;
+        Vector3 p2Pos = Manager.Obj.P2.transform.position;
 
+        Vector3 targetPos = Vector3.Lerp(p1Pos, p2Pos, 0.5f);
+        targetPos.y = 0f;
         transform.position = Vector3.Lerp(transform.position, targetPos, _moveSpeed * Time.deltaTime);
+
+        float dist = Mathf.Abs(p1Pos.x - p2Pos.x);
+
+        float size = Mathf.Lerp(size1, size2, Mathf.Clamp01((dist - dist1) / (dist2 - dist1)));
+        _camera.orthographicSize = size;
     }
 }
