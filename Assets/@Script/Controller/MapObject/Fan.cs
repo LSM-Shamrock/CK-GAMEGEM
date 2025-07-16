063e1921abb796574e240a00044d5bc615e09435
LSM-Shamrock <lsm.shamrock@gmail.com>
2025-07-17 05:30:28 +0900

    바람/기포 생성기

---
+++ Assets/@Script/Controller/MapObject/Fan.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Fan : MonoBehaviour
+{
+    [SerializeField] private float _power = 40f;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent(out Rigidbody2D rb) && rb.bodyType == RigidbodyType2D.Dynamic)
+        {
+            rb.linearVelocityY += _power;
+        }
+    }
+}
