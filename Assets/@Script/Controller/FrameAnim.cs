16b8fd82babb6bdd4aa3a8eefb2a3a9b4164296c
LSM-Shamrock <lsm.shamrock@gmail.com>
2025-07-17 01:49:41 +0900

    애니메이션 등

---
+++ Assets/@Script/Controller/FrameAnim.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class FrameAnim : MonoBehaviour
+{
+    [SerializeField] private Sprite[] _sprites;
+    [SerializeField] private float _frameSec = 0.1f;
+
+    private SpriteRenderer _sr;
+
+    private void Awake()
+    {
+        _sr = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnEnable()
+    {
+        StartCoroutine(PlayAnim());
+    }
+
+    private IEnumerator PlayAnim()
+    {
+        while (true)
+        {
+            foreach (Sprite sprite in _sprites)
+            {
+                _sr.sprite = sprite;
+                yield return new WaitForSeconds(_frameSec);
+            }
+        }
+    }
+}
