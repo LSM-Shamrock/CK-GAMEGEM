3b3c405883093a207123784a9896bb3705042846
LSM-Shamrock <lsm.shamrock@gmail.com>
2025-07-16 20:48:13 +0900

    시작씬 등

---
+++ Assets/@Script/UI/Scene/StartCanvas.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartCanvas : UI_Scene
+{
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene("GameScene");
+        }
+    }
+}
