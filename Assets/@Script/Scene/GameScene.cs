20b35b756fdeecf00452af38697eae08ab5d99b7
LSM-Shamrock <lsm.shamrock@gmail.com>
2025-07-17 02:42:43 +0900

    ㅁ

--- Assets/@Script/Scene/GameScene.cs
+++ Assets/@Script/Scene/GameScene.cs
@@ -4,10 +4,14 @@ public class GameScene : BaseScene
 {
     [SerializeField] private BoxCollider2D _progressArea;
 
+    private float _goalX;
+
     protected override bool Init()
     {
         if (base.Init() == false)
             return false;
+
+        _goalX = _progressArea.bounds.max.x;
 
         Manager.Game.LevelCount = FindObjectsByType<SavePoint>(FindObjectsSortMode.None).Length;
         Manager.Game.CurLevel = 0;
@@ -23,5 +27,10 @@ public class GameScene : BaseScene
     private void Update()
     {
         Manager.Game.Sec += Time.deltaTime;
+
+        if (Manager.Game.P1.transform.position.x > _goalX && Manager.Game.P2.transform.position.x > _goalX)
+        {
+
+        }
     }
 }
