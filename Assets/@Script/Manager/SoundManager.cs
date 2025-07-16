d59f23651227aae93136c6955ed59ebd4c02182f
LSM-Shamrock <lsm.shamrock@gmail.com>
2025-07-17 06:04:06 +0900

    사운드매니저

---
+++ Assets/@Script/Manager/SoundManager.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+
+public class SoundManager
+{
+    private AudioSource _bgmPlayer;
+    private AudioSource BgmPlayer
+    {
+        get
+        {
+            if (_bgmPlayer == null)
+            {
+                _bgmPlayer = new GameObject("BGM").AddComponent<AudioSource>();
+                GameObject.DontDestroyOnLoad(_bgmPlayer.gameObject);
+                _bgmPlayer.loop = true;
+            }
+            return _bgmPlayer;
+        }
+    }
+
+    private AudioSource _sfxPlayer;
+    private AudioSource SfxPlayer
+    {
+        get
+        {
+            if (_sfxPlayer == null)
+            {
+                _sfxPlayer = new GameObject("SFX").AddComponent<AudioSource>();
+                GameObject.DontDestroyOnLoad(_sfxPlayer.gameObject);
+            }
+            return _sfxPlayer;
+        }
+    }
+
+    private float _masterVolume;
+    private float _sfxVolume;
+    private float _bgmVolume;
+
+    public float MasterVolume
+    {
+        get { return _masterVolume; }
+        set
+        {
+            _masterVolume = value;
+            SfxPlayer.volume = _sfxVolume * _masterVolume;
+            BgmPlayer.volume = _bgmVolume * _masterVolume;
+        }
+    }
+    public float SfxVolume
+    {
+        get { return _sfxVolume; }
+        set
+        {
+            _sfxVolume = value;
+            SfxPlayer.volume = _sfxVolume * _masterVolume;
+        }
+    }
+    public float BgmVolume
+    {
+        get { return _bgmVolume; }
+        set
+        {
+            _bgmVolume = value;
+            BgmPlayer.volume = _bgmVolume * _masterVolume;
+        }
+    }
+
+    public void PlaySFX(AudioClip audioClip)
+    {
+        SfxPlayer.PlayOneShot(audioClip);
+    }
+
+    public void SetBGM(AudioClip audioClip)
+    {
+        if (audioClip == BgmPlayer.clip)
+            return;
+
+        BgmPlayer.clip = audioClip;
+        if (audioClip != null)
+            BgmPlayer.Play();
+        else
+            BgmPlayer.Stop();
+    }
+}
