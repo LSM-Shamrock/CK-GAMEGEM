186d89e1e6f6f7964f6dd401a94fa5ff25f9e06d
LSM-Shamrock <lsm.shamrock@gmail.com>
2025-07-17 11:46:35 +0900

    타이틀 캔버스

--- Assets/@Script/UI/Scene/TitleCanvas.cs
+++ Assets/@Script/UI/Scene/TitleCanvas.cs
@@ -3,29 +3,32 @@ using UnityEngine.SceneManagement;
 
 public class TitleCanvas : UI_Scene
 {
+    public enum Buttons
+    {
+        PlayButton,
+        RankingBUtton,
+    }
+
     public override bool Init()
     {
         if (base.Init() == false)
             return false;
 
+        BindButton(typeof(Buttons));
 
-
-        return true;
-    }
-
-    private void Update()
-    {
-        if (Input.GetMouseButtonDown(0))
+        GetButton((int)Buttons.PlayButton).onClick.AddListener(() =>
         {
             SceneManager.LoadScene("GameScene");
-        }
+        });
 
-        if (Input.GetKeyDown(KeyCode.R) && Manager.UI.PeekPopupUI<UI_Popup>() == null)
+        GetButton((int)Buttons.RankingBUtton).onClick.AddListener(() =>
         {
             Manager.UI.ShowPopupUI<RankingPop>(callback: (rankingPop) =>
             {
 
             });
-        }
+        });
+
+        return true;
     }
 }
