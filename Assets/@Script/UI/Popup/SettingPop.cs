a720bcb0293db4864a24ae41476635101edfe52f
LSM-Shamrock <lsm.shamrock@gmail.com>
2025-07-17 14:41:24 +0900

    설정 UI

--- Assets/@Script/UI/Popup/SettingPop.cs
+++ Assets/@Script/UI/Popup/SettingPop.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.TextCore.Text;
 
 public class SettingPop : UI_Popup
 {
@@ -7,12 +8,33 @@ public class SettingPop : UI_Popup
         VolumeSlider,
     }
 
-    public enum Images
+    public enum Buttons
     {
-        Vol0,
-        Vol25,
-        Vol50,
-        Vol100,
+        ScreenModeL,
+        ScreenModeR,
+    }
+
+    public enum TextPros
+    {
+        ScreenModeText,
+    }
+
+    public enum Objects
+    {
+        VolumeButtonGroup,
+    }
+
+    private bool IsFullScreen
+    {
+        get { return Screen.fullScreen; }
+        set
+        {
+            Screen.fullScreen = value;
+            if (Screen.fullScreen == true)
+                Screen.fullScreenMode = FullScreenMode.FullScreenWindow;
+            else
+                Screen.fullScreenMode = FullScreenMode.Windowed;
+        }
     }
 
     public override bool Init()
@@ -21,11 +43,37 @@ public class SettingPop : UI_Popup
             return false;
 
         BindSlider(typeof(Sliders));
-        BindImage(typeof(Images));
+        BindButton(typeof(Buttons));
+        BindTextPro(typeof(TextPros));
+        BindObject(typeof(Objects));
 
         GetSlider((int)Sliders.VolumeSlider).onValueChanged.AddListener((value) =>
         {
             Manager.Sound.MasterVolume = value;
+        });
+        Transform volumeButtonGroup = GetObject((int)Objects.VolumeButtonGroup).transform;
+        int volumeButtonCount = volumeButtonGroup.childCount;
+        for (int i = 0; i < volumeButtonCount; i++)
+        {
+            int idx = i;
+            BindEvent(volumeButtonGroup.GetChild(idx).gameObject, () =>
+            {
+                float volume = (float)idx / (volumeButtonCount - 1);
+                GetSlider((int)Sliders.VolumeSlider).value = volume;
+                Manager.Sound.MasterVolume = volume;
+            });
+        }
+
+        GetTextPro((int)TextPros.ScreenModeText).text = $"창 모드 : {(IsFullScreen ? "전체화면" : "창")}";
+        GetButton((int)Buttons.ScreenModeL).onClick.AddListener(() =>
+        {
+            IsFullScreen = !IsFullScreen;
+            GetTextPro((int)TextPros.ScreenModeText).text = $"창 모드 : {(IsFullScreen ? "전체화면" : "창")}";
+        });
+        GetButton((int)Buttons.ScreenModeR).onClick.AddListener(() =>
+        {
+            IsFullScreen = !IsFullScreen;
+            GetTextPro((int)TextPros.ScreenModeText).text = $"창 모드 : {(IsFullScreen ? "전체화면" : "창")}";
         });
 
         return true;
